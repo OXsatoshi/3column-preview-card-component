4c59295f1c45db6f5b63e5fb3c78e8fb2c9479fa
Nabil Afia <nabil.afia.afia@gmail.com>
2026-08-07 02:30:53 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "3column-preview-card-component",
+  base: "/3column-preview-card-component",
 });
